5fc2572023c12a707b23683adf6b9dca1f116a70
clement <derochclement@gmail.com>
2022-11-20 18:53:03 +0100

    test de variable local storage

--- front/js/product.js
+++ front/js/product.js
@@ -3,7 +3,6 @@ function getCurrentURL () {
   }
   const url = new URL(getCurrentURL());
   const idp = url.searchParams.get("id");
-  console.log(idp);
 
 function displayProduct(objet){
 
@@ -30,17 +29,55 @@ function displayProduct(objet){
   img.appendChild(newimg);
 }
 
+function saveCart(canap) {
+  // JSON.stringify prend l'objet et le transforme en chaine de caractere
+  localStorage.setItem("canap", JSON.stringify(canap));
+}
+
+function getCart() {
+  // recuperation de l'objet canap
+  let canap = localStorage.getItem("canap");
+  // si canap et == a null
+  if(canap == null) {
+    // alors on retourne le tableau (l'objet)
+    return []
+    //sinon on retransform l'objet en string 
+  }else{
+    // JSON.parse retransforme la chaine de caractere "string" en objet
+    return canap;
+    return JSON.parse(canap)
+  }
+}
+
+function addToCart(idp, quantity, color) {
+  // recuperatrion du panier dans le local storage
+  let canap = getCart();
+  console.log(idp, quantity, color)
+  // cherche dans le panier si il y as un produit donc l'id et = a l'id du produit ajouter
+  let foundProduct = canap.findIndex((p) => p.id === idp && p.color === color);
+  // si le produit et différent de undefined
+  if(foundProduct != undefined) {
+    //l'objet et deja dans le panier donc on ajoute une quantiter
+    let quantiter = canap[foundProduct].quantity;
+    quantiter = quantiter+quantity;
+  }else{
+    //défini la quantiter par défaut a 1.
+    const produit = {
+      id: idp,
+      color: color,
+      quantity: quantity,
+    }
+    // ajout du produit
+    canap.push(produit);
+  }
+  saveCart(canap);
+}
+
 const button = document.getElementById('addToCart');
 
 button.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log(title)
-  const canap = {
-    id: idp,
-    color: colors.value,
-    quantity: quantity.value
-  }
-  localStorage.setItem("canap", JSON.stringify(canap));
+  addToCart(idp, quantity.value, colors.value);
 });
 
 
